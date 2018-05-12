ab46abb685634454ceab88479e3a5ffb3d8ba43f
Caleb Foust <cfoust@sqweebloid.com>
2018-05-11 18:41:31 -0700

    1-bit-and-2-bit-characters: final solution

--- problems/1-bit-and-2-bit-characters/solution.py
+++ problems/1-bit-and-2-bit-characters/solution.py
@@ -1,16 +1,21 @@
+# This approach just "hops" over the second bit in a character since both of
+# the two-bit characters start with a one. If we end up at the last element
+# in the array, it means we hopped onto it. Since it's the only element left
+# it cannot be the start of a two-bit character. Otherwise, if the last
+# element in the array is a two-bit character, we will "hop" to the last
+# index (which is the cardinality of the input.)
+
 class Solution:
     def isOneBitCharacter(self, bits):
-        i = 0
+        last = len(bits) - 1
 
-        while i < len(bits):
+        i = 0
+        while i < last:
             if bits[i]:
                 i += 1
             i += 1
 
-            if i == len(bits) - 1:
-                return True
-
-        return False
+        return i == last
 
 s = Solution()
 
