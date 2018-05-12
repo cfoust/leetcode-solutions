4ff1c139be7350d805170c85d995101e585cd426
Caleb Foust <cfoust@sqweebloid.com>
2018-05-11 18:36:05 -0700

    1-bit-and-2-bit-characters: bad solution

--- problems/1-bit-and-2-bit-characters/solution.py
+++ problems/1-bit-and-2-bit-characters/solution.py
@@ -1,6 +1,20 @@
 class Solution:
     def isOneBitCharacter(self, bits):
-        """
-        :type bits: List[int]
-        :rtype: bool
-        """
+        i = 0
+
+        while i < len(bits):
+            if bits[i]:
+                i += 1
+            i += 1
+
+            if i == len(bits) - 1:
+                return True
+
+        return False
+
+s = Solution()
+
+print(s.isOneBitCharacter([1, 0, 0]))
+print(s.isOneBitCharacter([1, 1, 1, 0]))
+print(s.isOneBitCharacter([1, 1, 1, 0, 0, 0, 0]))
+print(s.isOneBitCharacter([1, 1, 1, 0, 0, 0, 0, 1, 1]))
